7b004c8fa3a9f6aa15931a05fff0bb3c716b2425
Turists <ervins.tripans@gmail.com>
2016-01-21 08:34:03 +0200

    Facebook login

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Debril\RssAtomBundle\DebrilRssAtomBundle(),
+            new CodeMe\TheBundle\TheBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
